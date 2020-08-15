9a78671e6a81ad9f02832947d8fa2629bcff701f
thamtran <thamtran9x@gmail.com>
2020-08-15 16:26:36 +0200

    sending login request

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,12 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -37,5 +38,13 @@ export class AuthService {
     }));
   }
 
- 
+ login(email: string, password: string) {
+    // tslint:disable-next-line:max-line-length
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCWq_2qULObFwaF8r8w41NgM_u5bq8HAFk', {
+      email,
+      password,
+      returnSecureToken: true
+      }
+    );
+  }
 }
