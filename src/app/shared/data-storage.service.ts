b744a487ea7edb670439087828822285999bc277
thamtran <thamtran9x@gmail.com>
2020-08-24 15:22:32 +0200

    auth guard

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
@@ -18,7 +18,7 @@ export class DataStorageService {
     const recipes = this.recipeService.getRecipes();
     this.http
       .put(
-        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json',
+        'https://angular-recipes-557ce.firebaseio.com/recipes.json',
         recipes
       )
       .subscribe(response => {
@@ -27,11 +27,11 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.http
+
+      return this.http
       .get<Recipe[]>(
-        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json'
-      )
-      .pipe(
+        'https://angular-recipes-557ce.firebaseio.com/recipes.json'
+      ).pipe(
         map(recipes => {
           return recipes.map(recipe => {
             return {
